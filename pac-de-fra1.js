fc7d117e9f9dac195d0dc56cdaba8cacf4065907
Hawkins Sherpherd <35891650+Hawkins-Sherpherd@users.noreply.github.com>
2024-08-08 16:27:36 +0800

    Create pac-de-fra1.js

---
+++ pac-de-fra1.js
@@ -0,0 +1,9 @@
+var tlds = [".dn42",".neo",".crxn"];
+
+function FindProxyForURL(url, host){
+  for(var i = 0; i < tlds.length; i++){
+    if (dnsDomainIs(host,tlds[i]){
+      return "SOCKS de-fra1-dn42-proxy.sherpherd.top:8081";
+    }
+  }
+}
